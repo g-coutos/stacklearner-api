32a0f38f28ac36c2085c5fe5031de16d51d2c8d2
gui couto <gcoutos95@gmail.com>
2025-03-17 16:27:28 -0300

    feat: adding pagination

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, session, jsonify
-from app.errors import ItemNotFoundError, ItemAlreadyExists
+from app.errors import PaginationValueError, ItemNotFoundError, ItemAlreadyExists
 from app.services.post_service import get_all, get_one, create, edit, delete
 
 post = Blueprint('post', __name__, url_prefix='/posts')
@@ -11,6 +11,7 @@ def handle_options():
 @post.route('/', methods=['GET'])
 def list_posts():
 	_id = request.args.get('_id')
+	page = request.args.get('page')
 
 	if _id:
 		try:
@@ -21,11 +22,21 @@ def list_posts():
 		except ItemNotFoundError as e:
 			return jsonify({
 				'error': str(e)
-			})
+			}), 404
 	else:
-		posts = get_all()
+		try:
+			posts = get_all(page)
+			
+			return jsonify({
+				'posts': [post.to_dict() for post in posts['posts']],
+				**{k: v for k, v in posts.items() if k != "posts"}
+			}), 200
 		
-		return jsonify([post.to_dict() for post in posts]), 200
+			
+		except PaginationValueError as e:
+			return jsonify({
+				'error': str(e)
+			}), 400
 
 @post.route('/', methods=['POST'])
 def create_post():
--- app/services/post_service.py
+++ app/services/post_service.py
@@ -1,10 +1,43 @@
 from app.models.post import Post
-from app.errors import ItemNotFoundError, ItemAlreadyExists
+from app.errors import PaginationValueError, ItemNotFoundError, ItemAlreadyExists
 from app.database import db_session
-import datetime as dt
 
-def get_all():
-	return Post.query.order_by(Post.created.desc()).all()
+import datetime as dt
+from math import ceil
+
+def get_all(page):
+	posts = Post.query.order_by(Post.created.desc()).all()
+
+	pagination_limit = 15
+	posts_total = len(posts)
+	pages_total = ceil(posts_total / pagination_limit)
+	
+	if posts_total < pagination_limit or page == None:
+		return {
+			'posts': posts,
+		}
+	
+	page = int(page)
+	
+	if page <= 0:
+		raise PaginationValueError("Page param can't be negative or zero")
+	
+	if page > pages_total:
+		raise PaginationValueError("This page does not exist")
+	
+	paginated_posts = posts[(page - 1) * pagination_limit:page * pagination_limit]
+	
+	if not (page * pagination_limit) < posts_total:
+		paginated_posts = posts[(page - 1) * pagination_limit:page * len(posts)]
+
+	return {
+		'posts': paginated_posts,
+		'posts_total': posts_total,
+		'pages_total': pages_total,
+		'page': page,
+		'has_previus_page': page > 1,
+		'has_next_page': (page * pagination_limit) < posts_total,
+	}
 
 def get_one(_id):
 	post = Post.query.get(_id)
