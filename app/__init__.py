59f6d104e7cd18bf11440a72292f47c447faaf11
gui couto <gcoutos95@gmail.com>
2025-05-28 21:09:16 -0300

    feat: adding variables treatment

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from dotenv import load_dotenv
 
 from flask import Flask
 from flask_session import Session
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from . import database, config
 from app import controllers
+import os
 
 load_dotenv()
 
@@ -13,8 +14,8 @@ def create_app():
 	app.config.from_object(config.Config)
 
 	Session(app)
-	CORS(app, supports_credentials=True)
-	
+	CORS(app, origins=[os.getenv("WEB_HOST")], supports_credentials=True)
+
 	database.init_db()
 	database.init_app(app)
 
